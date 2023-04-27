d4d68f0a01655d11540e4783f8f6035bf413d429
EL JABIRI Tarik <tarik.eljabiri@gmail.com>
2023-04-27 23:42:02 +0000

    Revert "feat: add projections and coordinates transform" (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,24 @@
-import { MantineProvider } from "@mantine/core";
+import {
+  MantineProvider,
+} from "@mantine/core";
 import Home from "./Home";
 
 function App() {
   return (
-    <MantineProvider
-      withGlobalStyles
-      withNormalizeCSS
-      theme={{
-        colorScheme: "light",
-        globalStyles: () => ({
-          "#map": {
-            height: "560px",
-          },
-          '.coordinates-control': {
-            margin: "0",
-            padding: "0 2px",
-            backgroundColor: 'white',
-            fontSize: "16px"
-          },
-          '.zones-control': {
-            fontSize: "16px",
-            padding: "2px"
-          }
-        }),
-      }}
-    >
-      <Home />
-    </MantineProvider>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'light',
+          globalStyles: () => ({
+            "#map": {
+              height: "520px",
+            },
+          }),
+        }}
+      >
+        <Home />
+      </MantineProvider>
   );
 }
 
--- src/Map.tsx
+++ src/Map.tsx
@@ -1,19 +1,35 @@
-import { Paper } from "@mantine/core";
+import { ActionIcon, Group, Paper } from "@mantine/core";
+import { IconFileExport, IconFileImport } from "@tabler/icons-react";
+import * as L from "leaflet";
 import { useRef, useEffect } from "react";
 import "leaflet/dist/leaflet.css";
-import { MapWidget } from "./map-widget";
 
 export default function Map() {
-  const mapContainerRef = useRef<HTMLDivElement | null>(null);
-  const mapWidgetRef = useRef<MapWidget | null>(null);
+  const mapMounted = useRef(false);
 
   useEffect(() => {
-    if (mapWidgetRef.current === null) {
-      if (mapContainerRef.current) {
-        mapWidgetRef.current = new MapWidget(mapContainerRef.current);
-      }
+    if (!mapMounted.current) {
+      const map = L.map("map").setView([51.505, -0.09], 13);
+
+      L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+      mapMounted.current = true;
     }
   }, []);
 
-  return <Paper ref={mapContainerRef} shadow="sm" id="map" />;
+  return (
+    <>
+      <Group mb={'xs'}  spacing="xs">
+        <ActionIcon variant="outline" size={"lg"}>
+          <IconFileExport />
+        </ActionIcon>
+        <ActionIcon variant="outline" size={"lg"}>
+          <IconFileImport />
+        </ActionIcon>
+      </Group>
+      <Paper shadow="sm" id="map"></Paper>
+    </>
+  );
 }
