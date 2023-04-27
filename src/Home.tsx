afe45b5261644dcb51ff0ab4747743bd0fdddeda
EL JABIRI Tarik <tarik.eljabiri@gmail.com>
2023-04-27 23:43:01 +0000

    Revert "Revert "feat: add projections and coordinates transform" (#2)"
    This reverts commit d4d68f0a01655d11540e4783f8f6035bf413d429.

--- src/Home.tsx
+++ src/Home.tsx
@@ -7,16 +7,18 @@ import Tools from "./Tools";
 export default function Home() {
   const [value, setValue] = useState("map");
   return (
-    <Container>
-      <SegmentedControl
-        mt={"xs"}
-        onChange={setValue}
-        data={[
-          { label: "Map", value: "map" },
-          { label: "Outils", value: "tools" },
-        ]}
-      />
-      <Divider my="sm" />
+    <Container size="xl" px="xs">
+      <div>
+        <SegmentedControl
+          mt={"xs"}
+          onChange={setValue}
+          data={[
+            { label: "Map", value: "map" },
+            { label: "Outils", value: "tools" },
+          ]}
+        />
+        <Divider my="sm" />
+      </div>
       {value === "map" ? <Map /> : <Tools />}
     </Container>
   );
